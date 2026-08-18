f44480c65a85c28a403394ca3fc90a6f49e13fc7
saurabh <saurabhmvp01@gmail.com>
2026-08-18 12:56:21 +0530

    lll

--- admin/src/constants/brand.js
+++
@@ -1,5 +0,0 @@
-export const LOGO_URL =
-  "https://res.cloudinary.com/dsafvwkrf/image/upload/v1780561447/GreenGrocc_logo_2-removebg-preview_wcso0k.png";
-
-export const STORE_URL =
-  import.meta.env.VITE_STORE_URL || "https://www.greengrocc.in/";
--- admin/src/utils/authStorage.js
+++
@@ -1,1 +0,0 @@
-export const ADMIN_STORAGE_KEY = "bmm_admin_auth";
